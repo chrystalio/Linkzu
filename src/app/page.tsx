ba70cb13b57c41dcd5c931fc4d0f9a1e9a5a2f87
Chrystalio <chrystalio.k@outlook.com>
2025-01-13 21:47:15 +0700

    Remove unused LinkCard component and its props definition from the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import Image from "next/image";
 import data from "../../data.json"
-
-interface LinkCardProps {
-  title: string;
-  href: string;
-}
+import LinkCard from "./components/LinkCard";
 
 function GitHubIcon() {
   return (
@@ -92,16 +88,5 @@ export default function Home() {
           <LinkCard key={link.title} title={link.title} href={link.href} />
         ))}
     </div>
-  );
-}
-
-function LinkCard({ title, href }: LinkCardProps) {
-  return (
-    <a href={href} target="_blank"
-    rel="noopener noreferrer" className="flex mt-3 text-sm items-center p-3 w-full rounded-md hover:scale-105 transition-all border mb-3 max-w-3xl shadow-md bg-gray-300 text-gray-800 duration-500">
-      <div className="flex flex-col text-center w-full">
-        <h2 className="font-semibold text-md">{title}</h2>
-      </div>
-    </a>
   );
 }
